7dc12ed89061786c05b52dccc70a58a97e68cf1b
Abdelrahmanmahjob <114360935+Abdelrahmanmahjob@users.noreply.github.com>
2025-08-09 14:52:44 +0300

    Update cofetti

--- App.jsx
+++ App.jsx
@@ -4,19 +4,6 @@ import { languages } from "./languages"
 import { getFarewellText, getRandomWord } from "./utils"
 import Confetti from "react-confetti"
 
-/**
- * Backlog:
- * 
- * ✅ Farewell messages in status section
- * ✅ Disable the keyboard when the game is over
- * ✅ Fix a11y issues
- * ✅ Choose a random word from a list of words
- * ✅ Make the New Game button reset the game
- * ✅ Reveal what the word was if the user loses the game
- * ✅ Confetti drop when the user wins
- * 
- * Challenge: 🎊🎊🎊🎊🎊
- */
 
 export default function AssemblyEndgame() {
     // State values
@@ -143,6 +130,8 @@ export default function AssemblyEndgame() {
             {
                 isGameWon && 
                     <Confetti
+                        width={window.innerWidth}
+                        height={window.innerHeight}
                         recycle={false}
                         numberOfPieces={1000}
                     />
